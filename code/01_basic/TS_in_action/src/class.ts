050449264c7f6c5fe239d6802de827bfb910ffea
13ruceYu <bruceyuhongbo@gmail.com>
2021-05-22 00:06:32 +0800

    stop for i while

--- code/01_basic/TS_in_action/src/class.ts
+++ code/01_basic/TS_in_action/src/class.ts
@@ -2,9 +2,17 @@ class Dog {
   constructor(name: string) {
     this.name = name
   }
-  name: string = 'dog'
+  public name: string = 'dog'
   run () {}
 }
 console.log(Dog.prototype)
 let dog = new Dog('wangwang')
 console.log(dog)
+
+class Husky extends Dog {
+  constructor(name: string, color: string) {
+    super(name);
+    this.color = color
+  }
+  color: string
+}
