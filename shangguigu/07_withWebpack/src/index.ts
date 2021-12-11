74e831b8cbd21b65861561323146cce0ae0fe69f
13ruceYu <bruceyuhongbo@gmail.com>
2021-12-11 23:59:07 +0800

    study: typescript webpack

---
+++ shangguigu/07_withWebpack/src/index.ts
@@ -0,0 +1,11 @@
+import { a } from './m'
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(3, 4));
+console.log('yooooooooooooo');
+console.log(a);
+
+
---
+++ shangguigu/07_withWebpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node-modules/
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+}
