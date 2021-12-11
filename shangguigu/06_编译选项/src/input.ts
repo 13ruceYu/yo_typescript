aa1473001a44fa8bc65e3760567b95d214b87d39
13ruceYu <bruceyuhongbo@gmail.com>
2021-12-11 16:12:27 +0800

    study: ts config

--- shangguigu/06_编译选项/src/input.ts
+++ shangguigu/06_编译选项/src/input.ts
@@ -4,3 +4,16 @@ let b: string[];
 b = ['hola'];
 console.log(mm);
 
+function fn(a: number, b: number) {
+  return a + b;
+}
+
+function fnn(this: Window) {
+  console.log(this)
+}
+
+const box = document.getElementById('box');
+box && box.addEventListener('click', () => {
+  console.log('clicked');
+})
+
