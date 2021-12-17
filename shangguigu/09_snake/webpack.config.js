ceddc9dd43dd271a1b0ea0679d77ee8e4acd4672
13ruceYu <bruceyuhongbo@gmail.com>
2021-12-17 22:48:36 +0800

    study: ts snake project config

---
+++ shangguigu/09_snake/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          // config babel
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      'chrome': '80',
+                      // 'ie': '11'
+                    },
+                    'corejs': '3',
+                    'useBuiltIns': 'usage'
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
+        exclude: /node-modules/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      browsers: 'last 2 versions'
+                    }
+                  ]
+                ]
+              }
+            }
+          },
+          'less-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+}
