c53001e56b96289a568d7730b730a70e0b39b69d
13ruceYu <bruceyuhongbo@gmail.com>
2021-12-18 23:56:38 +0800

    study: snake food class

--- shangguigu/09_snake/src/index.ts
+++ shangguigu/09_snake/src/index.ts
@@ -1,3 +1,32 @@
 import './style/index.less'
 
-console.log('hola world');
+// define food class
+class Food {
+  // food DOM
+  element: HTMLElement;
+
+  constructor() {
+    this.element = document.getElementById('food')!;
+  }
+
+  get X() {
+    return this.element.offsetLeft;
+  }
+
+  get Y() {
+    return this.element.offsetTop;
+  }
+
+  change() {
+    const left = Math.round(Math.random() * 29) * 10;
+    const top = Math.round(Math.random() * 29) * 10;
+
+    this.element.style.left = left + 'px';
+    this.element.style.top = top + 'px';
+  }
+}
+
+const food = new Food;
+food.change()
+console.log(food.element, food.X, food.Y);
+
